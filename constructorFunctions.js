5d2dd8767dfa728440f2480fef5dd764113fe3a7
Lewis Betances <patience174@hotmail.com>
2018-11-02 08:32:17 -0400

    1

--- constructorFunctions.js
+++ constructorFunctions.js
@@ -200,6 +200,21 @@ let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle);//true
 
 
+//Understand the prototype chain_________________________________________________________________________________________
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // => true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
 
 
 
