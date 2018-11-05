e1f09e319b175308aa9bf4a62692f227104ec13e
Lewis Betances <patience174@hotmail.com>
2018-11-04 23:55:05 -0500

    10

---
+++ IIFE.js
@@ -0,0 +1,8 @@
+(function () {
+  console.log("Chirp, chirp!");
+})(); // this is an anonymous function expression that executes right away
+// Outputs "Chirp, chirp!" immediately
+
+
+(function() {console.log("A cozy nest is ready");})
+();
